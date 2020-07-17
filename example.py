bad1a373498174a1841c190e840b0370199a3ede
Sergio <sergio@adtuo.com>
2020-07-17 19:21:43 +0200

    mongonator: updating example

--- example.py
+++ example.py
@@ -1,4 +1,3 @@
-# from pymongo import MongoClient
 import re
 
 from mongonator import MongoClientWithPagination
@@ -12,11 +11,6 @@ if __name__ == '__main__':
     db = mongo_client[db]
     col = db[col]
 
-    # a = col.paginate(limit=1, projection={'campaign_name': 1, '_id': 0}, ordering=-1)
-    # print(a.response)
-    # a = col.paginate(limit=1, projection={'campaign_name': 1, '_id': 0}, ordering=-1, next_page=a.next_page)
-    # print(a.response)
-
     query = {'campaign_name': {'$ne': None}}
     # query = {'email': re.compile(re.escape('ca'), re.IGNORECASE),
     #          'form_id': re.compile(re.escape('55158'), re.IGNORECASE)}
@@ -24,18 +18,13 @@ if __name__ == '__main__':
     #                  {'form_id': re.compile(re.escape('54'), re.IGNORECASE)}]}
     # query = {'campaign_name': None}
     # query = None
+
     ordering = 1
-    a = col.paginate(query=query, limit=1, projection={'email': 1, 'form_id': 1, '_id': 0},
-                     ordering_field='email', ordering=ordering)
-    print(a.response)
-    print(a.batch_size)
-    b = 1
-    while a.next_page:
-        a = col.paginate(query=query, limit=1, projection={'email': 1, 'form_id': 1, '_id': 0},
-                         ordering_field='email', next_page=a.next_page, ordering=ordering)
-        b += 1
-        print(a.response)
-        print(a.batch_size)
-    print("TOTAL ", b)
+
+    for d in col.paginate(query=query, limit=1, projection={'email': 1, 'form_id': 1, '_id': 0},
+                          ordering_field='email', ordering=ordering):
+        print(d.response)
+        print(d.batch_size)
+
     for i in col.find(query, projection={'email': 1, 'form_id': 1}).sort('email', 1):
         print(i)
