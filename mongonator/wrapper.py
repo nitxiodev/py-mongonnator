d4bc0ee45fd87894a14b12e8f9bd1b419f49681e
nitxiodev <smnitxio@gmail.com>
2021-05-03 21:36:24 +0200

    mongonator: adding response formatters as is or chat

--- mongonator/wrapper.py
+++ mongonator/wrapper.py
@@ -1,21 +1,26 @@
 class BaseResponseWrapper:
-    def operation(self, iterable: list):
+    def __init__(self, iterable: list):
+        self._iterable = iterable
+
+    def format(
+        self,
+    ):
         raise NotImplementedError("Implement in child classes")
 
-    def __call__(self, iterable: list):
-        """Make an operation to cursor/list"""
-        return self.operation(iterable)
 
-
-class AsIsWrapper(BaseResponseWrapper):
+class DefaultResponseWrapper(BaseResponseWrapper):
     """Wrapper to return response as is"""
 
-    def operation(self, iterable: list):
-        return iterable
+    def format(
+        self,
+    ):
+        return self._iterable
 
 
-class ChatWrapper(BaseResponseWrapper):
+class ChatResponseWrapper(BaseResponseWrapper):
     """Wrapper to return response showing the most recent value at bottom"""
 
-    def operation(self, iterable: list):
-        return iterable[::-1]
+    def format(
+        self,
+    ):
+        return self._iterable[::-1]
